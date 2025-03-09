8118621513dfad8d440d61f8e5ce0126b08271d6
n3m01726 <kiocubic.rulezz@proton.me>
2025-03-09 15:59:59 -0400

    Correction de la référence du bot à client dans la fonction de mise à jour du statut

--- tasks/updateStatus.js
+++ tasks/updateStatus.js
@@ -1,19 +1,16 @@
-const axios = require('axios');
-const { JSON_URL } = require('../config'); // Assurez-vous d'importer l'URL du JSON à partir de la config
-
 module.exports = {
   name: 'updateStatus',
   description: 'Mise à jour du statut du bot',
-  async execute(bot) {
+  async execute(client) {  // client ici au lieu de bot
     try {
       const { data } = await axios.get(JSON_URL);
       const currentSong = data.icestats.source.title || "No song available";
 
-      await bot.user.setActivity(`🎧 ${currentSong}`, { type: 'LISTENING' });
+      await client.user.setActivity(`🎧 ${currentSong}`, { type: 'LISTENING' });
       console.log(`Updated status: ${currentSong}`);
     } catch (error) {
       console.error('Error updating status:', error);
-      await bot.user.setActivity('Soundshine Radio', { type: 'LISTENING' });
+      await client.user.setActivity('Soundshine Radio', { type: 'LISTENING' });
     }
   },
 };
