ace8577beaeee4a83dfedaa2f939f9c7d10c120b
n3m01726 <kiocubic.rulezz@proton.me>
2025-04-05 22:13:52 -0400

    config.js update

--- config.js
+++ config.js
@@ -1,13 +1,19 @@
 require('dotenv').config();  // Si tu utilises dotenv
 module.exports = {
-    BOT_TOKEN: process.env.BOT_TOKEN,
-    UNSPLASH_ACCESS_KEY: process.env.UNSPLASH_API,
-    STREAM_URL: "https://stream.soundshineradio.com:8445/stream",
-    JSON_URL: "https://stream.soundshineradio.com:8445/status-json.xsl",
-    BOT_ROLE_NAME: "soundSHINE Radio",
-    VOICE_CHANNEL_ID: process.env.VOICE_CHANNEL_ID,
-    ADMINBOT_CHANNEL_ID: process.env.ADMINBOT_CHANNEL_ID,
-    ADMIN_ROLE_ID: process.env.ADMIN_ROLE_ID,
-    ANNOUNCEMENTS_CHANNEL_ID: process.env.ANNOUNCEMENTS_CHANNEL_ID,
-    PREFIX: '!s'
+
+    // Discord Token and informations
+BOT_TOKEN: process.env.BOT_TOKEN,
+BOT_ROLE_NAME: "soundSHINE Radio",
+VOICE_CHANNEL_ID: process.env.VOICE_CHANNEL_ID,
+ANNOUNCEMENTS_CHANNEL_ID: process.env.ANNOUNCEMENTS_CHANNEL_ID,
+PREFIX: '!s',
+
+// Unsplash API
+UNSPLASH_ACCESS_KEY: process.env.UNSPLASH_API,
+
+// Icecast API URLS
+STREAM_URL: "https://stream.soundshineradio.com:8445/stream",
+JSON_URL: "https://stream.soundshineradio.com:8445/status-json.xsl"    
+
 };
+
